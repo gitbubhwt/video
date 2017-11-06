3abd9635809523a78013718f315a931971ed3ece
hwt <xiongjiehu@126.com>
2017-11-06 18:28:06 +0800

    edit

--- http_server/start.go
+++ http_server/start.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	addr := ":5020"
 	//log.Info(http.Dir("/http_server/html/video.html"))
-	//http.Handle("/", http.FileServer(http.Dir("./http_server/")))
+	http.Handle("/", http.FileServer(http.Dir("./http_server")))
 	http.HandleFunc(route.ROUTE_HEAD_REQUEST, handle_video.VideoHeadHtml)
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
